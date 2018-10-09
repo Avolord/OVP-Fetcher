95091b6ce4ed639c3a7ca8091afa6c7d20796cb4
avolord <the.avo.lord@gmail.com>
2018-10-09 21:54:03 +0200

    removed useless files

--- src/Test.java
+++
@@ -1,9 +0,0 @@
-
-public class Test {
-	public static void main(String[] args) {
-		AvoFetch fetch = new AvoFetch("url");
-		fetch.loadOVP();
-		fetch = new AvoFetch("url");
-		fetch.loadOVP();
-	}
-}
